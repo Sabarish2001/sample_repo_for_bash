0904264223c07d6e8b00fc70920071181ff89edc
Sabarish2001 <sabarishb2001@gmail.com>
2023-11-14 20:46:14 +0530

    Added few details to my script

--- first_script.sh
+++ first_script.sh
@@ -5,4 +5,10 @@ read my_name
 echo "Whats your age dear?"
 read my_age
 echo "Welcome $my_name.Happy learning."
-
+echo "Wanna ask you few more details."
+echo "Where are you from?"
+read place
+echo "What you do for a living?"
+read living
+echo "Whats your dream ?"
+read dream
