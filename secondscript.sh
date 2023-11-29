c24ebb5fdcdcf07ce238fc4ab9988aee1f57464c
Sabarish2001 <sabarishb2001@gmail.com>
2023-11-29 20:49:06 +0530

    made changes to second script

--- secondscript.sh
+++ secondscript.sh
@@ -6,6 +6,9 @@ read num
 if [ $num -eq 100 ]
 then
 	echo "Cool.Both are equal."
+elif [ $num -ge 50 ] && [ $num -le 100 ]
+then
+	echo "They are present between the specified range"
 else
-	echo "OOPSy.They both are not equal."
+	echo "I am sorry.Out of range and not equal"
 fi
